73950eec0409c0feee2d513244fcf9a88e30a0d0
Rejana1234 <rejana.akter@gmail.com>
2022-05-08 15:25:42 +0600

    temp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
+import { FooterOverlay } from './components';
 import './App.css';
 
 const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
+  <div className="page-container">
+    <div className="content-wrap">
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <FooterOverlay />
+    </div>
   </div>
 );
 
--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 
 import './FooterOverlay.css';
 
-const FooterOverlay = () => (
-  <div>
-    FooterOverlay
-  </div>
-);
+const FooterOverlay = () => {
+  return(
+    <div className="main-footer">
+      <div className="container">
+        <div className=" row ">
+          {/*Column1 */}
+          <div className="col">
+            <h4>ghfvkj</h4>
+            <ul className="list-unstyled">
+              <li>2478980</li>
+              <li>fgfjk</li>
+              <li>werf hjkl</li>
+            </ul>
+          </div>
+          {/*Column2 */}
+          <div className="col">
+            <h4>ghfvkj</h4>
+            <ul className="list-unstyled">
+              <li>2478980</li>
+              <li>fgfjk</li>
+              <li>werf hjkl</li>
+            </ul>
+          </div>
+          {/*Column3 */}
+          <div className="col">
+            <h4>ghfvkj</h4>
+            <ul className="list-unstyled">
+              <li>2478980</li>
+              <li>fgfjk</li>
+              <li>werf hjkl</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+  }
 
 export default FooterOverlay;
